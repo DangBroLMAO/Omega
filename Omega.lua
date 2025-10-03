e071fb6802d23078f58dc32fc9e3eb88a7f5bac4
DangBroLMAO <epichubontop@gmail.com>
2025-10-03 15:08:01 -0700

    Update Omega.lua

--- Omega.lua
+++ Omega.lua
@@ -1,1 +1,7 @@
+--[[
+Obfuscatored by Dev
+discord: https://discord.gg/vbHVdNsq9n
+2025 Omega Hub®
+]]
+
 loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/21f76faa9a86399ea052a507f16b41bc"))()
